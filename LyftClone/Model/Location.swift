d0497617ea45f99f223d16d588dad9882822a213
Jose Carlos <josealarconchacon@gmail.com>
2021-02-12 13:05:53 -0500

    first commit of LyftClone

---
+++ LyftClone/Model/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  LyftClone
+//
+//  Created by Jose Alarcon Chacon on 2/12/21.
+//
+
+import Foundation
+
+class Location {
+    var title: String
+    var subtitle: String
+    let lat: Double
+    let lng: Double
+    
+    init(title: String, subtitle: String, lat: Double, lng: Double) {
+        self.title = title
+        self.subtitle = subtitle
+        self.lat = lat
+        self.lng = lng
+    }
+}
