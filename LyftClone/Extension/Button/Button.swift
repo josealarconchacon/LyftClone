72530c7f64cc5a1714d857a4e1b8da59a0515f7d
Jose Carlos <josealarconchacon@gmail.com>
2021-02-15 12:16:27 -0500

    search bar layout

--- LyftClone/Extension/Button/Button.swift
+++ LyftClone/Extension/Button/Button.swift
@@ -10,5 +10,9 @@ import UIKit
 extension UIButton {
     func search_bar() {
         layer.cornerRadius = 10.0
+        layer.shadowRadius = 1.0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        layer.shadowOpacity = 0.5
     }
 }
