a3dba5f82f987066c451c3f7520ceb6153fc25b2
Jose Carlos <josealarconchacon@gmail.com>
2021-02-15 11:51:49 -0500

    extension folder

---
+++ LyftClone/Controller/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  LyftClone
+//
+//  Created by Jose Alarcon Chacon on 2/15/21.
+//
+
+import Foundation
---
+++ LyftClone/Extension/Button/Button.swift
@@ -0,0 +1,14 @@
+//
+//  Button.swift
+//  LyftClone
+//
+//  Created by Jose Alarcon Chacon on 2/15/21.
+//
+
+import UIKit
+
+extension UIButton {
+    func search_bar() {
+        layer.cornerRadius = 10.0
+    }
+}
