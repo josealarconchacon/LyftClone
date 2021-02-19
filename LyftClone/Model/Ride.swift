4de613962859d7ba9344c77d237bea6424ef9174
Jose Carlos <josealarconchacon@gmail.com>
2021-02-19 14:45:38 -0500

    class Ride

--- LyftClone/Model/Ride.swift
+++ LyftClone/Model/Ride.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// Ride file is the data that user see when selecting a ride
+class Ride {
+    var thumbnail: String
+    let name: String
+    let capacity: String
+    let price: Double
+    let time: Date
+    
+    init(thumbnail: String, name: String, capacity: String, price: Double, time: Date) {
+        self.thumbnail = thumbnail
+        self.name = name
+        self.capacity = capacity
+        self.price = price
+        self.time = time
+    }
+}
