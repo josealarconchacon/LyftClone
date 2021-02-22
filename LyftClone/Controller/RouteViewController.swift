5af0388976945169d59b5fc9cb14e0477445f959
Jose Carlos <josealarconchacon@gmail.com>
2021-02-22 11:43:08 -0500

    set up data in tableView

--- LyftClone/Controller/RouteViewController.swift
+++ LyftClone/Controller/RouteViewController.swift
@@ -14,29 +14,49 @@ class RouteViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var selecteRideButton: UIButton!
     
+    var pickupLocation: Location?
+    var dropoffLocation: Location?
+    var ride = [Ride]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        
         view_button_layout()
+        location()
     }
     
     func view_button_layout() {
+        // rounding all the corners
         routeLableView.layer.cornerRadius = 10.0
         backButton.layer.cornerRadius = backButton.frame.size.width / 2.0
         selecteRideButton.layer.cornerRadius = 10.0
     }
+    
+    func location() {
+        let location = LocationService.share.get_recent_location()
+        pickupLocation = location[0]
+        dropoffLocation = location[1]
+        
+        ride = RideService.share.getRide(pickupLocation: pickupLocation!, dropoffLocation: dropoffLocation!)
+    }
+    
+    
     @IBAction func selectedRideButtonDidTaped(_ sender: UIButton) {
     }
 }
 
 extension RouteViewController: UITableViewDataSource, UITableViewDelegate  {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return ride.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RideCell") as! RideCell
+        
+        let rides = ride[indexPath.row]
+        cell.update(ride: rides)
         return cell
     }
 }
